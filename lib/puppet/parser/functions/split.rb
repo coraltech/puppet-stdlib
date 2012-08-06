64d952b9a76012f5d0c9343a21fc7684df5129e9
Adrian Webb <adrian.webb@venturiscm.com>
2012-08-06 00:37:15 -0400

    Adding a split function that converts strings to arrays.

---
+++ lib/puppet/parser/functions/split.rb
@@ -0,0 +1,22 @@
+#
+# Derived from: https://github.com/camptocamp/puppet-common/blob/master/lib/puppet/parser/functions/split.rb
+#
+# split($string, $delimiter) : $string
+# split($string[], $delimiter) : $string[][]
+#
+# Split the first argument(s) on every $delimiter. $delimiter is interpreted as
+# Ruby regular expression.
+#
+# For long-term portability it is recommended to refrain from using Ruby's
+# extended RE features.
+module Puppet::Parser::Functions
+  newfunction(:split, :type => :rvalue) do |args|
+    if args[0].is_a?(Array)
+      args.collect do |arg| 
+        arg.split(/#{args[1]}/) 
+      end
+    else
+      args[0].split(/#{args[1]}/)
+    end
+  end
+end
